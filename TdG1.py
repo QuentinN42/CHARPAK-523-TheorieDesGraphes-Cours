d4fff1d3145fad67acea4bf13003c8c4f693b199
QuentinN42 <quentin.lieumont@gmail.com>
2019-10-21 16:07:39 +0200

    black

--- TdG1.py
+++ TdG1.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 n: int = int(input("n = "))
-m = np.zeros((n,n))
+m = np.zeros((n, n))
 
 for i in range(n):
     for j in range(n):
@@ -12,7 +12,7 @@ for i in range(n):
             if v not in [0, 1]:
                 print("value must be 0 or 1")
             else:
-                m[i,j] = v
+                m[i, j] = v
 
 print(m)
 
