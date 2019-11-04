3a2cd47fea5939ca83f552ce8f9d136b0151b5f3
QuentinN42 <quentin.lieumont@gmail.com>
2019-11-04 17:36:27 +0100

    work in one way

--- turtle_prof.py
+++ turtle_prof.py
@@ -1,31 +1,60 @@
 from math import sqrt
-from turtle import *
+import turtle
+import numpy as np
+
+t = turtle.Turtle()
+
+
+def move_to(x: int, y: int, write: bool = False):
+    if write is False:
+        t.pendown()
+    t.setheading(0)
+    t.setpos(x, y)
+    t.pendown()
 
 
 def sommet(c, x, y):
-    up()
-    setpos(x, y)
-    down()
-    circle(15)
-    up()
-    setpos(x, y + 5)
-    write(c, False, align="center", font=("Ariel", 10, "normal"))
+    t.setheading(0)
+    t.up()
+    t.setpos(x, y)
+    t.down()
+    t.circle(15)
+    t.up()
+    t.setpos(x, y + 5)
+    t.write(c, False, align="center", font=("Ariel", 10, "normal"))
 
 
-def arc(x, y, z, t):
-    up()
-    setpos(z, t + 30)
-    left(90)
-    down()
-    circle(sqrt((x - z) * (x - z) + (y - t) * (y - t)) / 2, 180)
+def arc(x, y, a, b):
+    t.setheading(0)
+    t.up()
+    t.setpos(a, b + 30)
+    t.left(90)
+    t.down()
+    t.circle(sqrt((x - a) * (x - a) + (y - b) * (y - b)) / 2, 180)
 
 
-clearscreen()
-sommet(10, 50, 200)
-sommet(20, 300, 200)
-arc(50, 200, 300, 200)
+def arcp(x, y, a, b, p):
+    t.setheading(0)
+    t.up()
+    t.setpos(a, b + 30)
+    t.left(90)
+    t.down()
+    t.circle(sqrt((x - a) * (x - a) + (y - b) * (y - b)) / 2, 90)
+    t.write(p, align="center")
+    t.circle(sqrt((x - a) * (x - a) + (y - b) * (y - b)) / 2, 90)
 
 
-def graph(m):
-    for i in range(len(m)):
-        sommet(i, i * 100, i * 100)
+def graph(m: np.ndarray):
+    r = range(len(m))
+    for i in r:
+        sommet(i, i * 100, 0)
+    for i in r:
+        for j in r:
+            if m[i, j] != 0:
+                arcp(i * 100, 0, j * 100, 0, m[i, j])
+
+
+if __name__ == "__main__":
+    graph(np.ones((3, 3)))
+
+    input()
