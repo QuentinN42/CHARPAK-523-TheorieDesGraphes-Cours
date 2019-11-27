cf3c2d5d5351a198f1a36fba00f02189e6f0fbd4
QuentinN42 <quentin.lieumont@gmail.com>
2019-11-27 17:50:40 +0100

    chemin

--- pcc.py
+++ pcc.py
@@ -1,31 +1,35 @@
 import numpy as np
 
-INF = 10000
+INF = 100
 
 
 def djk(m: np.ndarray, deb: int):
     n = len(m)
-    r = np.arange(n)
+    r = np.arange(n).astype(int)
     p = np.zeros(n).astype(bool)
-    d = np.array([INF for i in r])
+    d = INF * np.ones(n).astype(int)
     d[deb] = 0
+    ch = np.zeros(n).astype(int)
     while 0 in p:
-        a = (d + np.array([INF for i in r]) * p).argmin()
+        a = np.where(p, INF, d).argmin()
         p[a] = True
         for b in r[p == False]:
-            d[b] = min(d[b], d[a] + m[a, b])
-    return d
+            if d[b] > d[a] + m[a, b]:
+                d[b] = d[a] + m[a, b]
+                ch[b] = a + 1
+    return d, ch
 
 
 if __name__ == "__main__":
     mat = np.array(
         [
             [0, -3, 0, 3, 0],
-            [-3, 4, 0, 0, 0],
-            [0, 0, 0, 0, 5],
-            [3, 0, 0, 0, -4],
+            [-3, 0, 0, 2, 4],
+            [0, 0, 0, 0, 1],
+            [3, 2, 0, 0, -4],
             [0, 4, 1, -4, 0]
         ]
-    )
-    line = 3
+    ).astype(int)
+    line = 2
+    mat = np.where(mat == 0, INF, mat)
     print(djk(mat, line))
